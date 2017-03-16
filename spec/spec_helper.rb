bb8fd692af047f67d7a4bff8ee2da55b05e7a12c
Dominic Sayers <dominic@sayers.cc>
2017-03-16 11:32:04 +0000

    Setup for Travis and Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+unless ENV['NO_SIMPLECOV']
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.start { add_filter '/spec/' }
+  Coveralls.wear! if ENV['COVERALLS_REPO_TOKEN']
+end
+
 require 'griddler/testing'
 require 'griddler/cloudmailin'
 
